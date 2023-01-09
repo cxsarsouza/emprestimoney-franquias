9d6150b2f1cec926f2220551fc743541ae3fd481
cxsarsouza <99675804+cxsarsouza@users.noreply.github.com>
2023-01-09 09:16:28 -0300

    upload inicial

---
+++ assets/js/theme.js
@@ -0,0 +1,222 @@
+/* Sticky Navigation */
+$(function() {
+  
+  var sticky = $('.sticky');
+  var contentOffset;
+  var nav_height;
+  
+  if (sticky.length) {
+    
+    if ( sticky.data('offset') ) {
+      contentOffset = sticky.data('offset');
+    }
+    else {
+      contentOffset = sticky.offset().top;
+    }
+    nav_height = sticky.height();
+  }
+  
+  var scrollTop = $(window).scrollTop();
+  var window_height = $(window).height();
+  var doc_height = $(document).height();
+  
+  $(window).bind('resize', function() {
+    scrollTop = $(window).scrollTop();
+    window_height = $(window).height();
+    doc_height = $(document).height();
+    navHeight();
+  });
+  
+  $(window).bind('scroll', function() {
+    stickyNav();
+  });
+  
+  function navHeight() {
+    sticky.css('max-height', window_height + 'px');
+  }
+  
+  function stickyNav() {
+    scrollTop = $(window).scrollTop();
+    if (scrollTop > contentOffset) {
+      sticky.addClass('fixed');
+    }
+    else {
+      sticky.removeClass('fixed');
+    }
+  }
+  
+});
+
+$('document').ready(function() {
+  var nav_height = 70;
+  
+  $("a[data-role='smoothscroll']").click(function(e) {
+    e.preventDefault();
+    
+    var position = $($(this).attr("href")).offset().top - nav_height;
+    
+    $("body, html").animate({
+      scrollTop: position
+    }, 1000 );
+    return false;
+  });
+});
+
+$('document').ready(function() {
+  // Back to top
+  var backTop = $(".back-to-top");
+  
+  $(window).scroll(function() {
+    if($(document).scrollTop() > 400) {
+      backTop.css('visibility', 'visible');
+    }
+    else if($(document).scrollTop() < 400) {
+      backTop.css('visibility', 'hidden');
+    }
+  });
+  
+  backTop.click(function() {
+    $('html').animate({
+      scrollTop: 0
+    }, 1000);
+    return false;
+  });
+});
+
+
+$('document').ready(function() {
+  
+  // Loader
+  $(window).on('load', function() {
+    $('.loader-container').fadeOut();
+  });
+  
+  // Tooltips
+  $('[data-toggle="tooltip"]').tooltip();
+  
+  // Popovers
+  $('[data-toggle="popover"]').popover();
+  
+  // Page scroll animate
+  new WOW().init();
+});
+
+
+$('document').ready(function() {
+  $('#bancos').owlCarousel({
+  loop:true,
+  margin:26,
+  dots:false,
+  autoplay:true,
+  autoplayTimeout:2500, 
+  animateOut: 'zoomOut',
+  animateIn: 'zoomIn',
+
+  responsive:{
+    0:{
+      items:1,
+      nav:false
+      
+  },
+  600:{
+      items:1,
+      nav:false
+  },
+  1000:{
+      items:1,
+      nav:false,
+  }
+  }
+});
+});
+
+
+$('document').ready(function() {
+  $('#difs').owlCarousel({
+  loop:true,
+  margin:26,
+  autoplay:true,
+  autoplayHoverPause: true,
+  animateOut: 'fadeOut',
+  animateIn: 'fadeIn',
+  responsiveClass: true,
+  responsive:{
+    0:{
+      items:1,
+      nav:false
+      
+  },
+  600:{
+    items:2,
+    nav:false
+  },
+  800:{
+      items:3,
+      nav:false
+  },
+  1000:{
+      items:4,
+      nav:false,
+  }
+  }
+});
+});
+
+
+$('document').ready(function() {
+  $('#testimonials').owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    animateOut: 'zoomOut',
+    animateIn: 'zoomIn',
+  });
+});
+
+
+$('document').ready(function() {
+  $('#diferenciais').owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    animateOut: 'zoomIn',
+    animateIn: 'zoomOut',
+  });
+});
+
+
+/*
+ *  Counter
+ *
+ *  Require(" jquery.animateNumber.min.js ", " jquery.waypoints.min.js ")
+ */
+$(document).ready(function() {
+  var counterInit = function() {
+    if ( $('.counter-section').length > 0 ) {
+      $('.counter-section').waypoint( function( direction ) {
+
+        if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+
+          var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
+          $('.number').each(function(){
+            var $this = $(this),
+              num = $this.data('number');
+            $this.animateNumber(
+              {
+                number: num,
+                numberStep: comma_separator_number_step
+              }, 5000
+            );
+          });
+          
+        }
+
+      } , { offset: '95%' } );
+    }
+
+  }
+
+  counterInit();
+});
