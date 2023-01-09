548da439297394d77405481066a9f113681249be
cxsarsouza <99675804+cxsarsouza@users.noreply.github.com>
2023-01-09 16:19:08 -0300

    mudanças

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -47,6 +47,8 @@ $(function() {
   
 });
 
+
+
 $('document').ready(function() {
   var nav_height = 70;
   
@@ -220,4 +222,33 @@ $(document).ready(function() {
   }
 
   counterInit();
+});
+
+$(window).on('load', function () {
+  if ($('.cover').length) {
+      $('.cover').parallax({
+          imageSrc: $('.cover').data('image'),
+          zIndex: '1'
+      });
+  }
+
+  $("#preloader").animate({
+      'opacity': '0'
+  }, 900, function () {
+      setTimeout(function () {
+          $("#preloader").css("visibility", "hidden").fadeOut();
+      }, 300);
+  });
+});
+
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop();
+  var box = $('.header-text').height();
+  var header = $('header').height();
+
+  if (scroll >= box - header) {
+      $("header").addClass("background-header");
+  } else {
+      $("header").removeClass("background-header");
+  }
 });
